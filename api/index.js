79ce9347adbb5820696a76bbd25a3b3dd0e5934f
Roshan Alexander <roshanalexander72@gmail.com>
2026-08-10 13:16:49 +0530

    Surface diagnostic errors with status 200 in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,40 +1,14 @@
-let app;
-let initError = null;
-
-try {
-  app = require('../server/server.js');
-} catch (err) {
-  initError = {
-    message: err.message,
-    stack: err.stack,
-    name: err.name
-  };
-  console.error('Init Error:', err);
-}
-
 module.exports = (req, res) => {
-  if (initError) {
-    return res.status(500).json({
-      success: false,
-      errorType: 'InitializationError',
-      message: initError.message,
-      stack: initError.stack
-    });
-  }
-
-  // Prepend /api if Vercel stripped /api prefix from req.url
-  if (req.url && !req.url.startsWith('/api')) {
-    req.url = '/api' + (req.url.startsWith('/') ? '' : '/') + req.url;
-  }
-
   try {
+    const app = require('../server/server.js');
     return app(req, res);
   } catch (err) {
-    return res.status(500).json({
+    return res.status(200).json({
       success: false,
-      errorType: 'RuntimeError',
-      message: err.message,
-      stack: err.stack
+      diagnosticError: true,
+      errorName: err.name,
+      errorMessage: err.message,
+      errorStack: err.stack
     });
   }
 };
