f410d1163cf33f77b593a321b2f8b6fd23aaf62a
Roshan Alexander <roshanalexander72@gmail.com>
2026-08-10 13:12:59 +0530

    Add try-catch block around Express require in api/index.js to surface exact serverless error

--- api/index.js
+++ api/index.js
@@ -1,5 +1,35 @@
-const app = require('../server/server.js');
+let app;
+let initError = null;
+
+try {
+  app = require('../server/server.js');
+} catch (err) {
+  initError = {
+    message: err.message,
+    stack: err.stack,
+    name: err.name
+  };
+  console.error('Init Error:', err);
+}
 
 module.exports = (req, res) => {
-  return app(req, res);
+  if (initError) {
+    return res.status(500).json({
+      success: false,
+      errorType: 'InitializationError',
+      message: initError.message,
+      stack: initError.stack
+    });
+  }
+
+  try {
+    return app(req, res);
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      errorType: 'RuntimeError',
+      message: err.message,
+      stack: err.stack
+    });
+  }
 };
