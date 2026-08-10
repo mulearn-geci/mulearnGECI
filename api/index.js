337d187e3390311579a29fe4ae0c4a91740b4e00
Roshan Alexander <roshanalexander72@gmail.com>
2026-08-10 13:10:59 +0530

    Surface exact serverless error JSON details in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,31 @@
+let app;
+let initError = null;
+
+try {
+  app = require('../server/server.js');
+} catch (err) {
+  initError = err;
+  console.error('Initialization error:', err);
+}
+
 module.exports = (req, res) => {
-  res.status(200).json({
-    success: true,
-    message: 'Vercel API is working!'
-  });
+  if (initError) {
+    return res.status(500).json({
+      success: false,
+      stage: 'init',
+      error: initError.message,
+      stack: initError.stack
+    });
+  }
+
+  try {
+    return app(req, res);
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      stage: 'handler',
+      error: err.message,
+      stack: err.stack
+    });
+  }
 };
