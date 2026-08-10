c02594416bcdf86703b3093f1508dedb7a4a2fd0
Roshan Alexander <roshanalexander72@gmail.com>
2026-08-10 13:09:32 +0530

    Remove root package.json type module to allow native Node CommonJS execution for Express backend on Vercel

--- api/index.js
+++ api/index.js
@@ -1,8 +1,5 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-
 const app = require('../server/server.js');
 
-export default function handler(req, res) {
-  return app(req, res);
-}
+module.exports = (req, res) => {
+  app(req, res);
+};
