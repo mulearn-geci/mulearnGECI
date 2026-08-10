c663d97b346e75f2c09d44d5e60278d5fdc7d360
Roshan Alexander <roshanalexander72@gmail.com>
2026-08-10 13:12:10 +0530

    Pass Vercel API requests to Express server router handler in api/index.js

--- api/index.js
+++ api/index.js
@@ -1,31 +1,5 @@
-let app;
-let initError = null;
-
-try {
-  app = require('../server/server.js');
-} catch (err) {
-  initError = err;
-  console.error('Initialization error:', err);
-}
+const app = require('../server/server.js');
 
 module.exports = (req, res) => {
-  if (initError) {
-    return res.status(500).json({
-      success: false,
-      stage: 'init',
-      error: initError.message,
-      stack: initError.stack
-    });
-  }
-
-  try {
-    return app(req, res);
-  } catch (err) {
-    return res.status(500).json({
-      success: false,
-      stage: 'handler',
-      error: err.message,
-      stack: err.stack
-    });
-  }
+  return app(req, res);
 };
